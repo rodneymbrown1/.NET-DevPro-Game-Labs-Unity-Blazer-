b704b67821dd2e66441d642c29275c395fc4fdfe
rodneymbrown1 <rodney.m.brown1@outlook.com>
2026-02-18 11:13:28 -0500

    implementing brazer with terminal asthetic ui

--- infra/src/Infra/InfraStack.cs
+++ infra/src/Infra/InfraStack.cs
@@ -15,7 +15,14 @@ public class InfraStack : Stack
         {
             BucketName = $"ai-game-portal-{accountId}",
             WebsiteIndexDocument = "index.html",
-            BlockPublicAccess = BlockPublicAccess.NONE,
+            WebsiteErrorDocument = "index.html",
+            BlockPublicAccess = new BlockPublicAccess(new BlockPublicAccessOptions
+            {
+                BlockPublicAcls = false,
+                BlockPublicPolicy = false,
+                IgnorePublicAcls = false,
+                RestrictPublicBuckets = false
+            }),
             ObjectOwnership = ObjectOwnership.BUCKET_OWNER_ENFORCED,
             RemovalPolicy = RemovalPolicy.DESTROY,
             AutoDeleteObjects = true
@@ -31,7 +38,7 @@ public class InfraStack : Stack
         // Deploy portal static files
         new BucketDeployment(this, "PortalDeployment", new BucketDeploymentProps
         {
-            Sources = new[] { Source.Asset("../portal") },
+            Sources = new[] { Source.Asset("../portal/bin/Release/net9.0/publish/wwwroot") },
             DestinationBucket = portalBucket
         });
 
@@ -40,7 +47,13 @@ public class InfraStack : Stack
         {
             BucketName = $"unity-game-builds-{accountId}",
             WebsiteIndexDocument = "index.html",
-            BlockPublicAccess = BlockPublicAccess.NONE,
+            BlockPublicAccess = new BlockPublicAccess(new BlockPublicAccessOptions
+            {
+                BlockPublicAcls = false,
+                BlockPublicPolicy = false,
+                IgnorePublicAcls = false,
+                RestrictPublicBuckets = false
+            }),
             ObjectOwnership = ObjectOwnership.BUCKET_OWNER_ENFORCED,
             RemovalPolicy = RemovalPolicy.DESTROY,
             AutoDeleteObjects = true,
@@ -61,6 +74,59 @@ public class InfraStack : Stack
             Resources = new[] { gameBucket.ArnForObjects("*") },
             Principals = new[] { new AnyPrincipal() }
         }));
+
+        // Deploy game build - non-Brotli files (index.html, loader.js, TemplateData, StreamingAssets)
+        new BucketDeployment(this, "GameBuildNonBrotli", new BucketDeploymentProps
+        {
+            Sources = new[] { Source.Asset("../games/mcp-unity-1-build-optimized", new Amazon.CDK.AWS.S3.Assets.AssetOptions
+            {
+                Exclude = new[] { "Build/*.br" }
+            }) },
+            DestinationBucket = gameBucket,
+            DestinationKeyPrefix = "mcp-unity-1"
+        });
+
+        // Deploy Brotli-compressed .wasm.br file with correct headers
+        new BucketDeployment(this, "GameBuildWasm", new BucketDeploymentProps
+        {
+            Sources = new[] { Source.Asset("../games/mcp-unity-1-build-optimized/Build", new Amazon.CDK.AWS.S3.Assets.AssetOptions
+            {
+                Exclude = new[] { "*.data.br", "*.framework.js.br", "*.loader.js" }
+            }) },
+            DestinationBucket = gameBucket,
+            DestinationKeyPrefix = "mcp-unity-1/Build",
+            ContentEncoding = "br",
+            ContentType = "application/wasm"
+        });
+
+        // Deploy Brotli-compressed .framework.js.br file with correct headers
+        new BucketDeployment(this, "GameBuildFramework", new BucketDeploymentProps
+        {
+            Sources = new[] { Source.Asset("../games/mcp-unity-1-build-optimized/Build", new Amazon.CDK.AWS.S3.Assets.AssetOptions
+            {
+                Exclude = new[] { "*.data.br", "*.wasm.br", "*.loader.js" }
+            }) },
+            DestinationBucket = gameBucket,
+            DestinationKeyPrefix = "mcp-unity-1/Build",
+            ContentEncoding = "br",
+            ContentType = "application/javascript"
+        });
+
+        // Deploy Brotli-compressed .data.br file with correct headers
+        // Increased memory/timeout/storage for the large data file (~75MB)
+        new BucketDeployment(this, "GameBuildData", new BucketDeploymentProps
+        {
+            Sources = new[] { Source.Asset("../games/mcp-unity-1-build-optimized/Build", new Amazon.CDK.AWS.S3.Assets.AssetOptions
+            {
+                Exclude = new[] { "*.wasm.br", "*.framework.js.br", "*.loader.js" }
+            }) },
+            DestinationBucket = gameBucket,
+            DestinationKeyPrefix = "mcp-unity-1/Build",
+            ContentEncoding = "br",
+            ContentType = "application/octet-stream",
+            MemoryLimit = 1024,
+            EphemeralStorageSize = Size.Mebibytes(512)
+        });
 
         // Stack Outputs
         new CfnOutput(this, "PortalUrl", new CfnOutputProps
