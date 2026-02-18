a2b3163178fd5c02a474428371f63c4682378199
rodneymbrown1 <rodney.m.brown1@outlook.com>
2026-02-18 12:51:21 -0500

    removed cdk from actions

--- infra/src/Infra/Program.cs
+++ infra/src/Infra/Program.cs
@@ -9,8 +9,8 @@ new InfraStack(app, stackName, env, new StackProps
 {
     Env = new Amazon.CDK.Environment
     {
-        Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT") ?? "668191889297",
-        Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION") ?? "us-east-1"
+        Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+        Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION")
     }
 });
 
